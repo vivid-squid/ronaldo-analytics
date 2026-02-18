bcca077343ea148bef85c322cb002004a4a09733
Barath Nithish L <barathnithish.offl@gmail.com>
2026-02-18 22:28:47 +0000

    minor

--- models/clean/clean_rejected_shots.sql
+++ models/clean/clean_rejected_shots.sql
@@ -5,3 +5,5 @@ select
     'NULL_SHOT_ID_NUMBER' as reject_reason
 from {{ ref('stg_shots_ronaldo_raw') }}
 where shot_id_number is null
+
+{{ dbt_utils.generate_surrogate_key(['ingestion_batch_id','match_id','shot_id_number','coalesce(match_event_id,0)']) }} as reject_id
