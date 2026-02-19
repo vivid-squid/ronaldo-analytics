955071d306a6e2ab1439ef8df0188df0a7fd3591
Barath Nithish L <114650303+vivid-squid@users.noreply.github.com>
2026-02-19 07:09:10 +0530

    Generate surrogate key for rejected shots
    Added surrogate key generation for rejected shots.

--- models/clean/clean_rejected_shots.sql
+++ models/clean/clean_rejected_shots.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='table') }}
+   
+{{ dbt_utils.generate_surrogate_key(['ingestion_batch_id','match_id','shot_id_number','coalesce(match_event_id,0)']) }} as reject_id
 
-select
+   select
     *,
     'NULL_SHOT_ID_NUMBER' as reject_reason
 from {{ ref('stg_shots_ronaldo_raw') }}
 where shot_id_number is null
-
-{{ dbt_utils.generate_surrogate_key(['ingestion_batch_id','match_id','shot_id_number','coalesce(match_event_id,0)']) }} as reject_id
