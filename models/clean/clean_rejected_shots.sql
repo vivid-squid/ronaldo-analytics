b6b6e2a228b42d05b231cb8a24b1d1393f842b21
Barath Nithish L <114650303+vivid-squid@users.noreply.github.com>
2026-02-19 07:26:41 +0530

    Fix SQL query to include match_event_id in surrogate key

--- models/clean/clean_rejected_shots.sql
+++ models/clean/clean_rejected_shots.sql
@@ -1,9 +1,13 @@
 {{ config(materialized='table') }}
-   
-{{ dbt_utils.generate_surrogate_key(['ingestion_batch_id','match_id','shot_id_number','coalesce(match_event_id,0)']) }} as reject_id
 
-   select
-    *,
-    'NULL_SHOT_ID_NUMBER' as reject_reason
+select
+  {{ dbt_utils.generate_surrogate_key([
+      'ingestion_batch_id',
+      'match_id',
+      'shot_id_number',
+      'match_event_id'
+  ]) }} as reject_id,
+  *,
+  'NULL_SHOT_ID_NUMBER' as reject_reason
 from {{ ref('stg_shots_ronaldo_raw') }}
 where shot_id_number is null
